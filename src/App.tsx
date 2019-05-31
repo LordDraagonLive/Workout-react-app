217cfa3e72d691ecc203ad8b3522f6dc837803e9
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-05-31 10:20:06 +0100

    combined the web and the mobile apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+// import * as React from 'react';
+// import { View, Text } from 'react-native';
 
-const App: React.FC = () => {
-  return (
-   <View>
-      <Text>Hello from react native web</Text>
-    </View>
-  );
-};
+// const App: React.FC = () => {
+//   return (
+//    <View>
+//       <Text>Hello from react native web</Text>
+//     </View>
+//   );
+// };
+
+// export default App;
+import { App } from '@lol/common';
 
 export default App;
